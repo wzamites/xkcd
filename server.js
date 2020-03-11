6ab3019a4ddad0c19efc28bd7e4be5b59161dc72
wzamites <bj_zamites@comcast.net>
2020-03-11 00:50:48 -0400

    add second api call on every click

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const https = require('https')
 const fetch = require('node-fetch');
 const app = express()
 const port = process.env.PORT || 8080
-
-const randIntGen = (max, min) => Math.floor(Math.random() * (max - min + 1) + min)
 
 class Server {
   constructor() {
     this.initExpressMiddleware()
     this.initRoutes()
     this.start()
+  }
+
+  getMax(url){
+  }
+
+  randomInteger(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
   initExpressMiddleware() {
@@ -21,14 +25,19 @@ class Server {
   initRoutes() {
     app.get('/api',
       (req, res) => {
-        const randomNumber = randIntGen(2278, 1)
-        const url = "https://xkcd.com/" + randomNumber + "/info.0.json"
-        fetch(url)
+        fetch("https://xkcd.com/info.0.json")
           .then(response => response.json())
           .then(data => {
-            res.send(JSON.stringify(data.img))
+            const max = Number(data.num)
+            const randomNumber = this.randomInteger(max, 1)
+            const url = "https://xkcd.com/" + randomNumber + "/info.0.json"
+            fetch(url)
+              .then(response => response.json())
+              .then(data => {
+                res.send(JSON.stringify(data.img))
+              })
+              .catch(data => JSON.stringify("https://xkcd.com/1/info.0.json"))
           })
-          .catch(data => JSON.stringify("https://xkcd.com/1/info.0.json"))
         })
     app.get('/index.js', (req, res) => res.sendFile(__dirname + "/static/index.js"))
     app.get('/index.css', (req, res) => res.sendFile(__dirname + "/static/index.css"))
