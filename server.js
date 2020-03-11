50a0693b3d9b592d57c748f3b67c5eff6866b033
wzamites <bj_zamites@comcast.net>
2020-03-10 20:39:41 -0400

    add port options for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser')
 const https = require('https')
 const fetch = require('node-fetch');
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
+
+const maxValueObject = {value: null, lastFetched: null}
 
 const randIntGen = (max, min) => Math.floor(Math.random() * (max - min + 1) + min)
 
@@ -14,29 +16,30 @@ class Server {
     this.start()
   }
 
-  start() {
-    app.listen(port, () => console.log("Server started on port " + port))
-  }
-
   initExpressMiddleware() {
     app.use(bodyParser.urlencoded({extended: true}))
   }
 
   initRoutes() {
-    app.get('/api', (req, res) => {
-      const randomNumber = randIntGen(2278, 1)
-      const url = "https://xkcd.com/" + randomNumber + "/info.0.json"
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          res.send(JSON.stringify(data.img))
+    app.get('/api',
+      (req, res) => {
+        const randomNumber = randIntGen(2278, 1)
+        const url = "https://xkcd.com/" + randomNumber + "/info.0.json"
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            res.send(JSON.stringify(data.img))
+          })
+          .catch(data => JSON.stringify("https://xkcd.com/1/info.0.json"))
         })
-        .catch(data => JSON.stringify("https://xkcd.com/1/info.0.json"))
-    })
     app.get('/index.js', (req, res) => res.sendFile(__dirname + "/static/index.js"))
     app.get('/index.css', (req, res) => res.sendFile(__dirname + "/static/index.css"))
     app.get('/', (req, res) => res.sendFile(__dirname + "/static/index.html"))
   }
+
+    start() {
+      app.listen(port, () => console.log("Server started on port " + port))
+    }
 }
 
 new Server()
